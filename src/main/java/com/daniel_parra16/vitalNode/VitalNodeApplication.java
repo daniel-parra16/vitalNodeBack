61ef2db558a702254ebdf6fea13a26c47e42dd10
daniel-parra16 <danielestebanparraruiz22@gmail.com>
2026-04-28 19:29:00 -0500

    Feat: Creacion de la estructura del proyecto primer commit

---
+++ src/main/java/com/daniel_parra16/vitalNode/VitalNodeApplication.java
@@ -0,0 +1,13 @@
+package com.daniel_parra16.vitalNode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel_parra16/vitalNode/VitalNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel_parra16.vitalNode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
